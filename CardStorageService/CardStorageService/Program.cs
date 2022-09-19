fbea95e30e847b36d2027b248d9103d4acc72e51
Xyliolol <93090627+Xyliolol@users.noreply.github.com>
2022-09-19 16:12:56 +0300

    Реализуйте сервис, в котором хранятся данные по дебетовым картам покупателей: 1. Реализуйте CRUD-методы. 2. Используйте EF.

--- CardStorageService/CardStorageService/Program.cs
+++ CardStorageService/CardStorageService/Program.cs
@@ -1,25 +1,75 @@
-var builder = WebApplication.CreateBuilder(args);
+using CardStorageService.Data;
+using CardStorageService.Services.Impl;
+using CardStorageService.Services;
+using Microsoft.AspNetCore.HttpLogging;
+using Microsoft.EntityFrameworkCore;
+using NLog.Web;
+using CardStorageService.Models;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace CardStorageService
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+
+            builder.Services.Configure<DatabaseOptions>(options =>
+            {
+                builder.Configuration.GetSection("Settings:DatabaseOptions").Bind(options);
+            });
+
+
+            builder.Services.AddHttpLogging(logging =>
+            {
+                logging.LoggingFields = HttpLoggingFields.All | HttpLoggingFields.RequestQuery;
+                logging.RequestBodyLogLimit = 4096;
+                logging.ResponseBodyLogLimit = 4096;
+                logging.RequestHeaders.Add("Authorization");
+                logging.RequestHeaders.Add("X-Real-IP");
+                logging.RequestHeaders.Add("X-Forwarded-For");
+            });
+
+            builder.Host.ConfigureLogging(logging =>
+            {
+                logging.ClearProviders();
+                logging.AddConsole();
+
+            }).UseNLog(new NLogAspNetCoreOptions() { RemoveLoggerFactoryFilter = true });
+
+
+            builder.Services.AddDbContext<CardStorageServiceDbContext>(options =>
+            {
+
+                options.UseSqlServer(builder.Configuration["Settings:DatabaseOptions:ConnectionString"]);
+            });
+
+
+            builder.Services.AddScoped<IClientRepositoryService, ClientRepository>();
+            builder.Services.AddScoped<ICardRepositoryService, CardRepository>();
+
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseAuthorization();
+            app.UseHttpLogging();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
---
+++ CardStorageService/CardStorageService/Services/Impl/ClientRepository.cs
@@ -0,0 +1,60 @@
+﻿using CardStorageService.Data;
+
+namespace CardStorageService.Services.Impl
+{
+    public class ClientRepository : IClientRepositoryService
+    {
+
+        private readonly CardStorageServiceDbContext _context;
+        private readonly ILogger<ClientRepository> _logger;
+
+
+        public ClientRepository(
+            ILogger<ClientRepository> logger,
+            CardStorageServiceDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public int Create(Client data)
+        {
+            _context.Clients.Add(data);
+            _context.SaveChanges();
+            return data.ClientId;
+        }
+
+        public void Delete(int id)
+        {
+            try
+            {
+                _context.Remove(id);
+                _context.SaveChanges();
+            }
+            catch
+            {
+                return;
+            }
+        }
+
+        public IList<Client> GetAll()
+        {
+            var clients = _context.Clients.ToList();
+            return clients;
+        }
+        public void Update(Client data)
+        {
+            var clients = _context.Clients.FirstOrDefault(cl => cl.ClientId == data.ClientId);
+            clients.FirstName = data.FirstName;
+            clients.Surname = data.Surname;
+            clients.Patronymic = data.Patronymic;
+
+            _context.SaveChanges();
+        }
+        public Client GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
