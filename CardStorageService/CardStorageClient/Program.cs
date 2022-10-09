2dbc96d3a4d5efc744b6c0b44f018918d05dbab1
Xyliolol <93090627+Xyliolol@users.noreply.github.com>
2022-10-09 12:26:41 +0300

    Введение в gRPC.

---
+++ CardStorageService/CardStorageClient/Program.cs
@@ -0,0 +1,45 @@
+﻿using Grpc.Net.Client;
+using static CardStorageServiceProtos.CardService;
+using static CardStorageServiceProtos.ClientService;
+
+namespace CardStorageClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AppContext.SetSwitch("System.Net.Http.SocketHttpHandler.Http2UnencryptedSupport", true);
+           
+
+            using var channel = GrpcChannel.ForAddress("http://localhost:5001");
+
+            ClientServiceClient clientService = new ClientServiceClient(channel);
+
+            var createClientResponse = clientService.Create(new CardStorageServiceProtos.CreateClientRequest
+            {
+                FirstName = "Tester",
+                SurName = "TEST1",
+                Patronymic = "TEST2"
+            });
+
+            Console.WriteLine($"Client {createClientResponse.ClientId} created successfully.");
+
+            CardServiceClient cardService = new CardServiceClient(channel);
+
+            var getByClientIdResponse = cardService.GetByClientId(new CardStorageServiceProtos.GetByClientIdRequest
+            {
+                ClientId = 1
+            });
+
+            Console.WriteLine("Cards:");
+            Console.WriteLine("======");
+
+            foreach (var card in getByClientIdResponse.Cards)
+            {
+                Console.WriteLine($"{card.CardNo}; {card.Name}; {card.CVV2}; {card.ExpDate}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
